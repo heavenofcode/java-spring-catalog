fb3885f81a55685240c1faa6e5db55f80507e583
Imam Panji Winata <imam.winata@ibm.com>
2025-03-24 19:28:24 +0700

    init

---
+++ src/main/java/com/hoc/app/JavaCatalogRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.hoc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCatalogRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCatalogRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoc/app/JavaCatalogRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoc.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCatalogRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
